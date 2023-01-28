6e6277251847840afd65b7566ab4c0b23df832a0
crispr09 <contact2sachin11@gmail.com>
2023-01-28 14:06:49 +0530

    adding 2 more

--- src/main/java/java8progrms/Stream2Filer.java
+++ src/main/java/java8progrms/Stream2Filer.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Stream1Filer {
+public class Stream2Filer {
 
 	public static void main(String[] args) {
 
@@ -16,19 +16,12 @@ public class Stream1Filer {
 		marks.add(4);
 		marks.add(5);
 		System.out.println(marks);
-		List<Integer> li = marks.stream().filter(i->i%2!=0).collect(Collectors.toList());
-		System.out.println(li);
-		
-		
 		//
 		List<Integer> updatedMarks = marks.stream().map(i->i+5).collect(Collectors.toList());
 		System.out.println(updatedMarks);
 		
-		
-		
 		Integer lis =marks.stream().sorted(Collections.reverseOrder()).limit(2).skip(1).findFirst().get();
-		System.out.println(lis);
-	
+		System.out.println(lis);	
 	}
 	public void add5GraceMarks(ArrayList<Integer> marks) {
 		
---
+++ src/main/java/java8progrms/Stream3Map1.java
@@ -0,0 +1,22 @@
+package java8progrms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Stream3Map1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> marks = new ArrayList<Integer>();
+		marks.add(1);
+		marks.add(2);
+		marks.add(3);
+		marks.add(4);
+		marks.add(5);
+		System.out.println(marks);
+
+		Integer lis = marks.stream().sorted(Collections.reverseOrder()).limit(2).skip(1).findFirst().get();
+		System.out.println(lis);
+	}
+
+}

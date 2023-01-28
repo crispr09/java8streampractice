01e178266eeed44223919de7a514b884fc2b6e33
crispr09 <contact2sachin11@gmail.com>
2023-01-28 15:53:03 +0530

    2

--- src/main/java/java8progrms/Stream12Sorted7.java
+++ src/main/java/java8progrms/Stream12Sorted7.java
@@ -1,8 +1,6 @@
 package java8progrms;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 /**
--- src/main/java/java8progrms/Stream13Sorted8IncreasingLength.java
+++ src/main/java/java8progrms/Stream13Sorted8IncreasingLength.java
@@ -1,6 +1,7 @@
 package java8progrms;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 /**
@@ -8,24 +9,36 @@ import java.util.stream.Collectors;
  * order sorting using comparator example- natural sorting
  *
  */
-public class Stream11Sorted6 {
+public class Stream13Sorted8IncreasingLength {
 
 	public static void main(String[] args) {
 
 		ArrayList<String> marks = new ArrayList<String>();
+		marks.add("Wayn");
 		marks.add("Kuma");
 		marks.add("Raja");
 		marks.add("Steve");
 		marks.add("Jackson");
 		marks.add("Richard");
 		marks.add("Shobhit");
-		marks.add("Williom");
-		marks.add("Wayn");
-		marks.add("Carlos");
+		marks.add("Williom D");
+
+		marks.add("Carlos Markos");
 		System.out.println(marks);
 		
-		List<String> lis2 = marks.stream().sorted((i,j) -> i.compareTo(j)).collect(Collectors.toList());
+		Comparator<String> c = (i,j) -> {
+			int l1 = i.length();
+			int l2 = j.length();
+			if(l1<l2) return -1;
+			else if(l1>l2) return 1;
+			else return 0;
+			
+		};
+		
+
+		List<String> lis2 = marks.stream().sorted(c).collect(Collectors.toList());
 		System.out.println(lis2);
+
 		
 		List<String> lis3 = marks.stream().sorted((i,j) -> j.compareTo(i)).collect(Collectors.toList());
 		System.out.println(lis3);
